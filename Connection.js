cb3d46c0cfc7bd50c4a6c5baa37aeb699b5acb6e
ChristianMinich <119011619+ChristianMinich@users.noreply.github.com>
2023-04-06 21:51:24 +0200

    database

--- Connection.js
+++ Connection.js
@@ -1,47 +1,12 @@
-const mariadb = require('mariadb');
-class Database{
+// myModule.js
 
-  constructor(){
-    createDatabase();
-    poolConnection();
-  }
+const mariadbSingleton = require('./DB-Singleton');
 
-
-static createDatabase() {
-  mariadb.createPool({
-    host: '127.0.0.1', 
-    user:'root', 
-    password: 'admin',
-    connectionLimit: 5
-});
-  
+async function queryDatabase() {
+  const connection = await mariadbSingleton.getConnection();
+  // Use the connection to execute queries
+  // ...
+  connection.release(); // Release the connection back to the pool
 }
 
-static poolConnection(){
-pool.getConnection()
-    .then(conn => {
-    
-      conn.query("SELECT 1 as val")
-        .then((rows) => {
-          console.log(rows); //[ {val: 1}, meta: ... ]
-          //Table must have been created before 
-          // " CREATE TABLE myTable (id int, val varchar(255)) "
-          return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
-        })
-        .then((res) => {
-          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
-          conn.end();
-        })
-        .catch(err => {
-          //handle error
-          console.log(err); 
-          conn.end();
-        })
-        
-    }).catch(err => {
-      //not connected
-    });
-
-  }
-
-}
+module.exports = { queryDatabase };
