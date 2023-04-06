0cf951c48dc3b9d8912cc9c6ad9d0e87937b5342
ChristianMinich <119011619+ChristianMinich@users.noreply.github.com>
2023-04-06 14:28:25 +0200

    Seller.sql

---
+++ Seller.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Seller(
+    name VARCHAR(255),
+    owner VARCHAR(255),
+    street VARCHAR(255),
+    house_number INT NOT NULL,
+    zip_code INT NOT NULL,
+    telephone_number INT NOT NULL,
+    e_mail NVARCHAR(320)
+);
