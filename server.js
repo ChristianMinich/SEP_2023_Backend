cb9c8e9c1afa23d94b799910fbe1ca181ec73a3b
ChristianMinich <119011619+ChristianMinich@users.noreply.github.com>
2023-04-07 16:39:17 +0200

    solve merge conflict

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const https = require('https');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const singleton = require('./Server-Singleton');
-//const singleton_db = require('./DB-Singleton');
+const singleton_db = require('./DB-Singleton');
 
 const express = require('express');
 const login = require('./routes/Login');
 const signup = require('./routes/Signup');
+const order = require('./routes/Order');
+const details = require('./routes/Store-Details');
 
 const app = singleton.getInstance();
 //const db = singleton_db.getInstance();
 
 app.use(express.json());
 
-// Start the server
-/*https.createServer(app).listen(3000, () =>{
-  console.log('Server started on port 3000');
-}); */
-
+// Default
 app.get('/',(req, res) =>{
     res.sendFile(__dirname + '/index.html');
 });
 
-//Hello World
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+//
+// Store Details
+//
+
+app.get('/store-details', details.store_details);
+
+//
+// Login
+//
 
 app.get('/login', (req, res) => {
   res.send('Please send your Login Information!')
@@ -34,6 +36,10 @@ app.get('/login', (req, res) => {
 // Define the login endpoint
 app.post('/login', login.login);
 
+//
+// Signup
+//
+
 app.get('/signup', (req, res) => {
   res.send('Please send your Signup Information!')
 })
@@ -41,12 +47,23 @@ app.get('/signup', (req, res) => {
 // Define the sign up endpoint
 app.post('/signup', signup.signup);
 
+//
 // 404 Not Found
+//
 
 app.get('*', (req, res) =>{
   res.send('404');
 })
 
-app.listen(3000, () =>{
-  console.log('server on 3000');
+
+//
+// ORDER POST
+//
+
+app.post('/order', order.order);
+
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
