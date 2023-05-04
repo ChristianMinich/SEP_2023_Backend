c155ba394790ce3317f565a2cb62baae4da90d2d
ChristianMinich <119011619+ChristianMinich@users.noreply.github.com>
2023-05-04 20:31:22 +0200

    Update

---
+++ Database-Test/all-users.js
@@ -0,0 +1,13 @@
+const database = require('../DB-Singleton');
+exports.user = function(req, res) {
+    const db = database.getConnection();
+
+    db.then(conn => {
+        conn.query('SELECT * FROM USER').then(rows => {
+            //console.log(rows);
+            res.status(200).json(rows);
+        });
+    });
+
+}
+
