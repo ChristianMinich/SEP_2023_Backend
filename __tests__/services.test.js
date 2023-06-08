2b4e8588b8086d88bd89ed08df72741d5a33f89d
raniamo8 <raneaxxl@gmail.com>
2023-06-08 17:59:28 +0200

    new tests with documenatation

--- __tests__/services.test.js
+++ __tests__/services.test.js
@@ -1,30 +1,89 @@
 
 const { ValidateEmail } = require("../services/E-Mail-Validator");
 
+/**
+ * This is the test section for the function ValidateEmail.
+ */
 describe("ValidateEmail", () => {
-    test("Valid email", () => {
+
+    /**
+     * Checks if the ValidateEmail function returns true for a valid email.
+     */
+    test("True: Valid email", () => {
         const email = "example@test.com";
         const result = ValidateEmail(email);
         expect(result).toBe(true);
     });
 
-
-    test("Invalid email: null pointer", () => {
+    /**
+     * Checks if the ValidateEmail function returns false for a null pointer.
+     */
+    test("False: null pointer", () => {
         const email = null;
         const result = ValidateEmail(email);
         expect(result).toBe(false);
     });
 
-
-    test("Invalid email: empty string", () => {
+    /**
+     * Checks if the ValidateEmail function returns false for an empty email.
+     */
+    test("False: empty string", () => {
         const email = "";
         const result = ValidateEmail(email);
         expect(result).toBe(false);
     });
 
+    /**
+     * Checks if the ValidateEmail function returns false for an email without an '@' symbol.
+     */
+    test("False: missing '@'", () => {
+        const email = "testexample.com";
+        const result = ValidateEmail(email);
+        expect(result).toBe(false);
+    });
 
-    test("Invalid email: invalid format", () => {
+    /**
+     * Checks if the ValidateEmail function returns false for an email without a domain.
+     */
+    test("False: missing domain", () => {
+        const email = "test@";
+        const result = ValidateEmail(email);
+        expect(result).toBe(false);
+    });
+
+    /**
+     * Checks if the ValidateEmail function returns false for an email without .com .
+     */
+    test("False: invalid format", () => {
         const email = "test@example";
+        const result = ValidateEmail(email);
+        expect(result).toBe(false);
+    });
+
+    /**
+     * Checks if the ValidateEmail function returns false for an email with a special character.
+     */
+    test("False: contains special character", () => {
+        const email = "test@exa!mple.com";
+        const result = ValidateEmail(email);
+        expect(result).toBe(false);
+    });
+
+
+    /**
+     * Checks if the ValidateEmail function returns false for an email without a dot in the domain.
+     */
+    test("False: no dot", () => {
+        const email = "test@examplecom";
+        const result = ValidateEmail(email);
+        expect(result).toBe(false);
+    });
+
+    /**
+     * Checks if the ValidateEmail function returns false for an email without a username.
+     */
+    test("False: no username", () => {
+        const email = "@example.com";
         const result = ValidateEmail(email);
         expect(result).toBe(false);
     });
